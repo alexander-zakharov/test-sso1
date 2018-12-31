e4d77d603b3a0eae77ada57933f385193fec87bb
Alexandr Zakharov <zalexs@ip-192-168-1-121.ec2.internal>
2018-12-31 17:23:42 -0500

    test

--- src/main/java/tech/libra/test/okta/App.java
+++ src/main/java/tech/libra/test/okta/App.java
@@ -1,16 +1,9 @@
 package tech.libra.test.okta;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import tech.libra.webfilter.esapi.SecurityWrapper;
 
 
 @SpringBootApplication
